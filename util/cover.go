11624686cfb54f491fcc04d41dd0a11a5e7a453d
loser <18846178039@163.com>
2022-05-21 17:29:30 +0800

    完成生成封面功能

--- util/cover.go
+++ util/cover.go
@@ -1,6 +1,29 @@
 package util
 
+import (
+	"os/exec"
+	"path"
+	"strings"
+)
+
+var DefaultCoverFilename = "cover.png"
+
 func MakeCover(filename, dirname string) string {
-	return "cover.png"
-	// todo 从视频抽帧生成图片
+	suffix := path.Ext(filename)
+	if suffix != ".mp4" {
+		return DefaultCoverFilename
+	}
+	coverFilename := strings.TrimSuffix(filename, suffix) + ".png"
+	coverFilePath := dirname + coverFilename
+	cmd := exec.Command("ffmpeg",
+		"-ss", "0",
+		"-t", "1",
+		"-i", dirname+filename,
+		"-vframes", "1",
+		coverFilePath)
+
+	if err := cmd.Run(); err != nil {
+		return DefaultCoverFilename
+	}
+	return coverFilename
 }
